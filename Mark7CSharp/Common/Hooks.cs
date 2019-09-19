f9f37e1b912fc063cc63ca1216053db1f3b4ac60
ilton <exit>
2019-09-18 21:39:12 -0300

    #master atualização projeto

---
+++ Mark7CSharp/Common/Hooks.cs
@@ -0,0 +1,28 @@
+﻿namespace Mark7CSharp.Common
+{
+    using MongoDB.Bson;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public sealed class Hooks
+    {
+       
+        [BeforeScenario]
+        [Scope(Tag = "add_task")]
+        public void BeforeScenario()
+        {
+            var mongo = new MongodbHelper();
+            var user = mongo.GetUserByEmail("ilton.io@ninja.com.br");
+            var id = user["_id"];
+
+            var tarefa = new BsonDocument
+            {
+                { "title", "Tarefa indesejada"},
+                {"dueDate", "31/12/2019" },
+                {"done", false }
+            };
+
+            mongo.CreateTask(tarefa);
+        }
+    }
+}
---
+++ Mark7CSharp/Common/MongodbHelper.cs
@@ -0,0 +1,47 @@
+﻿namespace Mark7CSharp.Common
+{
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using System.Configuration;
+
+    public class MongodbHelper
+    {
+        private IMongoDatabase _dataBase; 
+           
+        public MongodbHelper()
+        {
+            //*Host => endereco
+            //* Port => Porta
+            //* Database => Base 
+            //* User => usuario
+            //* Pass => senha
+
+            //Como => mongodb://User:Pass@Host:Port/Database
+            string strConn = "";
+            var client = new MongoClient(ConfigurationManager.AppSettings["MongoStrConn"]);
+            _dataBase = client.GetDatabase(ConfigurationManager.AppSettings["MonoDataBase"]);
+        }
+
+        private IMongoCollection<BsonDocument>  Collection(string collection)
+        {
+            return _dataBase.GetCollection<BsonDocument>(collection);
+        }
+
+        public BsonDocument GetUserByEmail(string email)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("profile.eamil", email);
+            return  Collection("users").Find(filter).First();
+        }
+
+        public void CreateTask(BsonDocument tarefa)
+        {
+            Collection("tasks").InsertOne(tarefa);
+        }
+
+        public void DeleteByTitle(string titulo)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("title", titulo);
+            Collection("tasks").DeleteMany(filter);
+        }
+    }
+}
